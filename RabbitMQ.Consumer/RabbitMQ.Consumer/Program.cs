ed32789ae01d29e77d1ee772de0e5f13670bc97d
Fahri Gedik <fahrican9978@gmail.com>
2025-02-20 14:03:04 +0300

    Create Publisher App

---
+++ RabbitMQ.Consumer/RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using RabbitMQ.Client;
+
+Console.WriteLine("Hello, RabbitMQ");
+
+ConnectionFactory factory = new()
+{
+    HostName = "localhost",
+    Port = 5672,
+    UserName = "guest",
+    Password = "guest"
+};
+
+
+
---
+++ RabbitMQ.Publisher/RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,45 @@
+﻿
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Exceptions;
+
+Console.WriteLine("Hello RabbitMQ");
+
+ConnectionFactory factory = new()
+{
+    HostName = "localhost",
+    Port = 5672,
+    UserName = "guest",
+    Password = "guest"
+};
+
+try
+{
+    // create a connection
+    using IConnection connection = await factory.CreateConnectionAsync();
+    Console.WriteLine("connection is created");
+
+    // create a channel
+    using IChannel channel = await connection.CreateChannelAsync();
+    Console.WriteLine("channel is created");
+
+    // declare a queue
+    //
+    await channel.QueueDeclareAsync(queue: "example-queue", exclusive: false);
+
+
+    // publish a message
+    //RabbitMQ kuyruğa atacağı mesajları byte türünden kabul etmektedir. Haliyle mesajları bizim byte'a dönüştürmemiz gerekmektedir.
+    byte[] messageBodyBytes = Encoding.UTF8.GetBytes("Hello, RabbitMQ!");
+
+    channel.BasicPublishAsync(exchange: "", routingKey: "example-queue", body: messageBodyBytes);
+    Console.WriteLine("message is published");
+
+    Console.WriteLine("RabbitMQ Connection ve Channel'ı açık tutmak için bekleme modu aktifleştirildi.");
+    Console.ReadLine();
+}
+catch (RabbitMQClientException ex)
+{
+    throw;
+}
+
