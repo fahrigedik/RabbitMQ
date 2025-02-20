f56a35890de2b43fa98da7082d32563338876aca
Fahri Gedik <fahrican9978@gmail.com>
2025-02-20 14:28:30 +0300

    Create Consumer

--- RabbitMQ.Consumer/RabbitMQ.Consumer/Program.cs
+++ RabbitMQ.Consumer/RabbitMQ.Consumer/Program.cs
@@ -1,9 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Text;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
 Console.WriteLine("Hello, RabbitMQ");
 
+//Bağlantı oluşturma
 ConnectionFactory factory = new()
 {
     HostName = "localhost",
@@ -12,5 +15,30 @@ ConnectionFactory factory = new()
     Password = "guest"
 };
 
+//Bağlantı aktifleştirme
+using IConnection connection = await factory.CreateConnectionAsync();
+Console.WriteLine("connection is created");
 
+//Kanal oluşturma
+using IChannel channel = await connection.CreateChannelAsync();
+Console.WriteLine("channel is created");
+
+//Kuyruk oluşturma
+await channel.QueueDeclareAsync(queue: "example-queue", exclusive: false);
+//Consumer'da da kuyruk publisher'daki ile birebir aynı yapılandırma da tanımlanmalıdır.
+
+
+
+AsyncEventingBasicConsumer consumer = new(channel);
+channel.BasicConsumeAsync(queue: "example-queue", false, consumer);
+
+consumer.ReceivedAsync += async (object sender, BasicDeliverEventArgs e) =>
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+    Console.WriteLine($"Gelen Mesaj: {message}");
+    await channel.BasicAckAsync(e.DeliveryTag, false);
+};
+
+
+Console.ReadLine();
 
--- RabbitMQ.Publisher/RabbitMQ.Publisher/Program.cs
+++ RabbitMQ.Publisher/RabbitMQ.Publisher/Program.cs
@@ -30,7 +30,7 @@ try
 
     // publish a message
     //RabbitMQ kuyruğa atacağı mesajları byte türünden kabul etmektedir. Haliyle mesajları bizim byte'a dönüştürmemiz gerekmektedir.
-    byte[] messageBodyBytes = Encoding.UTF8.GetBytes("Hello, RabbitMQ!");
+    byte[] messageBodyBytes = Encoding.UTF8.GetBytes("Senayı Çok Seviyoruuuuum!");
 
     channel.BasicPublishAsync(exchange: "", routingKey: "example-queue", body: messageBodyBytes);
     Console.WriteLine("message is published");
