537d97d7c6e9f085986c4af55215da87db9d3342
Fahri Gedik <fahrican9978@gmail.com>
2025-02-21 12:52:39 +0300

    Fanout Exchange

---
+++ FanoutExchange/FanoutExchange.Consumer2/FanoutExchange.Consumer2/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+Console.WriteLine("Hello, Fanout Exchange.");
+
+ConnectionFactory factory = new()
+{
+    HostName = "localhost",
+    Port = 5672,
+    UserName = "guest",
+    Password = "guest"
+};
+Console.WriteLine("Connection is created");
+
+await using IConnection connection = await factory.CreateConnectionAsync();
+Console.WriteLine("Connection is enabled");
+
+await using IChannel channel = await connection.CreateChannelAsync();
+Console.WriteLine("Channel is created");
+
+await channel.ExchangeDeclareAsync("fanout-exchange-example", ExchangeType.Fanout);
+
+await channel.QueueDeclareAsync("fanout-queue-example");
+await channel.QueueBindAsync(
+    "fanout-queue-example",
+    "fanout-exchange-example", "");
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+consumer.ReceivedAsync += async (sender, eventArgs) =>
+{
+    var body = eventArgs.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Message received: {message}");
+    await Task.CompletedTask;
+};
+
+
+await channel.BasicConsumeAsync(
+    queue: "fanout-queue-example",
+    autoAck: true, // Mesajlar otomatik acknowledge edilsin
+    consumer: consumer
+);
+Console.WriteLine("Started consuming messages...");
+Console.ReadLine(); // Uygulamanın kapanmaması için bekle
---
+++ FanoutExchange/FanoutExchange.Publisher/FanoutExchange.Publisher/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+using RabbitMQ.Client;
+
+
+Console.WriteLine("Hello, Fanout Exchange.");
+
+ConnectionFactory factory = new()
+{
+    HostName = "localhost",
+    Port = 5672,
+    UserName = "guest",
+    Password = "guest"
+};
+Console.WriteLine("Connection is created");
+
+await using IConnection connection = await factory.CreateConnectionAsync();
+Console.WriteLine("Connection is enabled");
+
+await using IChannel channel = await connection.CreateChannelAsync();
+Console.WriteLine("Channel is created");
+
+await channel.ExchangeDeclareAsync("fanout-exchange-example", ExchangeType.Fanout);
+Console.WriteLine("Exchange is declared");
+
+while (true)
+{
+    Console.WriteLine("Mesaj Giriniz : ");
+    string message = Console.ReadLine();
+    byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+
+    await channel.BasicPublishAsync("fanout-exchange-example", "", body: messageBytes);
+}
+
